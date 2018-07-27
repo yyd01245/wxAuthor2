2a94376c7f315cb86184be96481f295fa116253b
yandong.yan <yandong.yan@upai.com>
2018-07-27 18:57:32 +0800

    feat: add app

---
+++ pages/author2/author2.js
@@ -0,0 +1,90 @@
+// pages/author2/author2.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    show: "",
+    secrteKey:"DQDVAEDJAZNNJ23DF2E6PSFIAO5DPEB5"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+  scanCode: function () {
+    var that = this;
+    var show;
+    wx.scanCode({
+      success: (res) => {
+        this.show = "--result:" + res.result + "--scanType:" + res.scanType + "--charSet:" + res.charSet + "--path:" + res.path;
+        that.setData({
+          show: this.show
+        })
+        this.getCode();
+        wx.showToast({
+          title: '成功',
+          icon: 'success',
+          duration: 2000
+        })
+
+      }
+    });
+  },
+  getCode: function(){
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+
+})
